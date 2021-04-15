a2556e350f0db886fdd989e3af44cdbf463e0c19
Piotr Domian <domianpiotr111@gmail.com>
2021-04-15 20:13:38 +0200

    Task is done

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function isMaleName() {
-  // Zaimplementuj zadanie zgodnie z wytycznymi
+function isMaleName(name) 
+{
+  if (name.substr(-1) == "a"  || name.substr(-1) == "A" & name != "Bonawentura" & name != "BONAWENTURA")
+  {
+    return false;
+  }
+  else if (name.substr(-1) != "a" || name == "Bonawentura" || name == "BONAWENTURA")
+  {
+    return true;
+  }
 };
 
 module.exports = isMaleName;
