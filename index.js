8a912eda23427a700dac7d58f30bc123ce35500a
Piotr Domian <domianpiotr111@gmail.com>
2021-04-18 20:19:29 +0200

    Task is done

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function isMaleName(name) 
 {
-  if (name.substr(-1) == "a"  || name.substr(-1) == "A" & name != "Bonawentura" & name != "BONAWENTURA")
+  if ((name.substr(-1) == "a"  || name.substr(-1) == "A") & name != "Bonawentura" & name != "BONAWENTURA" & name != "bonawentura")
   {
     return false;
   }
-  else if (name.substr(-1) != "a" || name == "Bonawentura" || name == "BONAWENTURA")
+  else 
   {
     return true;
   }
